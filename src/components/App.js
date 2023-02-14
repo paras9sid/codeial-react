d5e3cac23bb682bb8afda454f5c33c75ce7343e5
paras9sid <sidharthjainp9@gmail.com>
2023-02-14 14:07:23 +0530

    Adding Routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,21 @@ import { getPosts } from "../api";
 import { Home } from "../pages";
 import Loader from "./Loader";
 import Navbar from "./Navbar";
-// import { Loader, Navbar } from './';
+import Login from "../pages/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const About = () => {
+  return <h1>About</h1>;
+};
+
+const UserInfo = () => {
+  return <h1>User</h1>;
+};
+
+const Page404 = () => {
+  return <h1>404 errorr</h1>;
+};
+
 function App() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -27,8 +41,16 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* props posts should be an array not sring - it will throw props validation error */}
-      <Home posts={posts} />  
+      <Router>
+        <Routes>
+          {/* props posts should be an array not sring - it will throw props validation error */}
+          <Route exact path="/" element={<Home posts={posts} />}></Route>
+          <Route path="/login" element={<Login />} exact></Route>
+          <Route path="/about" element={<About />} exact></Route>
+          <Route path="/user/aadadsad" element={<UserInfo />} exact></Route>
+          <Route path="/user/aadadsad" element={<Page404 />} exact></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
